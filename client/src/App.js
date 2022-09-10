1870b9492ca7e14ce77a1d3b4d6f0ff21e5b90b7
aidannairn <aidan.nairn95@gmail.com>
2022-09-11 11:54:46 +1200

    feat: Connect Apollo to React app.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+
 import Header from "./components/Header";
+
+const client = new ApolloClient({
+  url: process.env.REACT_APP_DEV_APOLLO_URL,
+  cache: new InMemoryCache()
+})
 
 const App = () => {
   return (
     <>
+    <ApolloProvider client={client}>
       <Header />
       <div className='container'>
         <h1>Hello World</h1>
       </div>
+    </ApolloProvider>
     </>
   )
 }
