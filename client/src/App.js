8852bd92b7836625f9893110c09285153ee4fca7
aidannairn <aidan.nairn95@gmail.com>
2022-09-11 12:07:18 +1200

    feat: Create Clients component with getClients query.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import Clients from './components/Clients';
 
 import Header from "./components/Header";
 
@@ -13,7 +14,7 @@ const App = () => {
     <ApolloProvider client={client}>
       <Header />
       <div className='container'>
-        <h1>Hello World</h1>
+        <Clients />
       </div>
     </ApolloProvider>
     </>
