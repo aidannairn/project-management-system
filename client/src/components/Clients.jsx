7b4cce000f5346dfbad11c69281ab827e61e369a
aidannairn <aidan.nairn95@gmail.com>
2022-09-11 12:58:16 +1200

    feat: Render each clients' details on a table row.

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,5 +1,7 @@
 import { gql, useQuery } from '@apollo/client'
 
+import ClientRow from './ClientRow'
+
 const GET_CLIENTS = gql`
   query getClients {
     clients {
@@ -22,7 +24,23 @@ const Clients = () => {
     <>
       {
         !loading && !error && (
-          <h1>Clients</h1>
+          <table className="table table-hover mt-3">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                data.clients.map(client => (
+                  <ClientRow key={client.id} client={client} />
+                ))
+              }
+            </tbody>
+          </table>
         )
       }
     </>
