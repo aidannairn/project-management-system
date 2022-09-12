31645e6699d01ebb6a5be67879c0d0e10df020a9
aidannairn <aidan.nairn95@gmail.com>
2022-09-12 14:32:36 +1200

    feat: Add Projects component that renders a ProjectCard component for each project in the database.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import Clients from './components/Clients';
 
 import AddClientModal from './components/AddClientModal';
+import Projects from './components/Projects';
 import Header from "./components/Header";
 
 const cache = new InMemoryCache({
@@ -35,6 +36,7 @@ const App = () => {
       <Header />
       <div className='container'>
         <AddClientModal />
+        <Projects />
         <Clients />
       </div>
     </ApolloProvider>
