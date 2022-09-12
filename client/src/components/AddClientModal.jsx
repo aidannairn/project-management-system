e053e40a92abd216c67f8e98f580b2507e0613d0
aidannairn <aidan.nairn95@gmail.com>
2022-09-12 15:41:02 +1200

    feat: Create AddProjectModal component and render on HomePage.

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -57,19 +57,19 @@ const AddClientModal = () => {
               <form onSubmit={onSubmit}>
                 <div className="mb-3">
                   <label className='form-label'>First Name</label>
-                  <input type="text" className="form-control" id="name" value={firstName} onChange={ e => setFirstName(e.target.value)} />
+                  <input type="text" className="form-control" id="firstName" value={firstName} onChange={ e => setFirstName(e.target.value)} />
                 </div>
                 <div className="mb-3">
                   <label className='form-label'>Last Name</label>
-                  <input type="text" className="form-control" id="name" value={lastName} onChange={ e => setLastName(e.target.value)} />
+                  <input type="text" className="form-control" id="lastName" value={lastName} onChange={ e => setLastName(e.target.value)} />
                 </div>
                 <div className="mb-3">
                   <label className='form-label'>Email</label>
-                  <input type="text" className="form-control" id="name" value={email} onChange={ e => setEmail(e.target.value)} />
+                  <input type="text" className="form-control" id="email" value={email} onChange={ e => setEmail(e.target.value)} />
                 </div>
                 <div className="mb-3">
                   <label className='form-label'>Phone</label>
-                  <input type="text" className="form-control" id="name" value={phone} onChange={ e => setPhone(e.target.value)} />
+                  <input type="text" className="form-control" id="phone" value={phone} onChange={ e => setPhone(e.target.value)} />
                 </div>
                 <button type="submit" data-bs-dismiss="modal" className="btn btn-secondary">Submit</button>
               </form>
