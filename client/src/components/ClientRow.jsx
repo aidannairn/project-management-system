81ba370b98d1c1a0c9fb683b9d3bb5262e85ca2b
aidannairn <aidan.nairn95@gmail.com>
2022-09-12 16:45:42 +1200

    feat: When a client is deleted. Refresh cached list of projects that are being rendered.

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -3,14 +3,15 @@ import { useMutation } from '@apollo/client'
 
 import { DELETE_CLIENT } from '../mutations/clientMutations'
 import { GET_CLIENTS } from '../queries/clientQueries'
+import { GET_PROJECTS } from '../queries/projectQueries'
 // import { GET_CLIENTS } from '../queries/clientQueries'
 
 const ClientRow = ({ client }) => {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    /* Refresh frontend after user has been removed (can slow down application is used too much)
-    refetchQueries: [{ query: GET_CLIENTS }] */
-    update(cache, { data: { deleteClient }}) {
+    // Refresh frontend after user has been removed (can slow down application is used too much)
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }]
+    /* update(cache, { data: { deleteClient }}) {
       const { clients } = cache.readQuery({
         query: GET_CLIENTS
       })
@@ -20,7 +21,7 @@ const ClientRow = ({ client }) => {
           clients: clients.filter(client => client.id !== deleteClient.id)
         }
       })
-    }
+    } */
   })
 
   return (
