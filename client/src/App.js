3d1ac5b05407cbb66b056929bff9883b828e5147
aidannairn <aidan.nairn95@gmail.com>
2022-09-12 14:40:04 +1200

    feat: Create Home page and route to it with react-router-dom.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
-import Clients from './components/Clients';
 
-import AddClientModal from './components/AddClientModal';
-import Projects from './components/Projects';
+import Home from './pages/Home';
 import Header from "./components/Header";
 
 const cache = new InMemoryCache({
@@ -33,12 +32,14 @@ const App = () => {
   return (
     <>
     <ApolloProvider client={client}>
-      <Header />
-      <div className='container'>
-        <AddClientModal />
-        <Projects />
-        <Clients />
-      </div>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
     </ApolloProvider>
     </>
   )
